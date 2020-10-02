cf5a1c986cf5a83ee5f95445df05499a9d4c21af
Barbara Sanchez <68566534+barbs-sanchez@users.noreply.github.com>
2020-10-02 13:02:23 -0500

    Add files via upload

---
+++ cuadro magico.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int n,aux,div;
+	int contador=1;
+	int cuadro[100][100];
+	printf("dime el tamaño del cuadro magico  ");
+	scanf("%d",&n);
+	div=n%2;
+    if (div==0)
+    {
+       printf("n no se puede crear");	
+	}
+	else
+	{
+		aux=n/2;
+		cuadro[1,aux+1]=1
+		for i=0; i<5; i++
+		{
+			
+		}
+	}
+	
+	
+	
+	
+	
+	return 0;
+}
